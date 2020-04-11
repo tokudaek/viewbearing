76b9663feab1f84382b51eead14708df3588019a
tokudaek <tokudaek@gmail.com>
2020-04-11 13:30:19 -0300

    projection point into line

--- src/main.py
+++ src/main.py
@@ -16,6 +16,8 @@ import matplotlib.pyplot as plt
 from multiprocessing import Pool
 from datetime import datetime
 import igraph
+import random
+from scipy.spatial.distance import cdist
 
 #############################################################
 def info(*args):
@@ -34,6 +36,7 @@ def get_4connected_neighbours_2d(i, j, n, thoroidal=False):
     Returns:
     ndarray 4x2: 4 neighbours indices
     """
+    info(inspect.stack()[0][3] + '()')
     inds = []
     if j > 0: # left
         inds.append([i, j-1])
@@ -58,7 +61,7 @@ def get_4connected_neighbours_2d(i, j, n, thoroidal=False):
     return np.array(inds)
 
 #############################################################
-def generate_lattice(n, thoroidal=False, s=10):
+def generate_lattice(n, thoroidal=False, edgearg=-1):
     """Generate 2d lattice of side n
 
     Args:
@@ -69,9 +72,11 @@ def generate_lattice(n, thoroidal=False, s=10):
     Returns:
     ndarray nx2, ndarray nxn: positions and adjacency matrix (triangular)
     """
+    info(inspect.stack()[0][3] + '()')
     n2 = n*n
     pos = np.ndarray((n2, 2), dtype=float)
     adj = np.zeros((n2, n2), dtype=int)
+    s = 1 / (n-1) if edgearg < 0 else edgearg
 
     k = 0
     for j in range(n):
@@ -90,17 +95,47 @@ def generate_lattice(n, thoroidal=False, s=10):
     return pos, adj
 
 #############################################################
-def create_dummy_graph():
-    n = 4
-    pos, adj = generate_lattice(n, False, s=1)
+def create_lattice(n, ax=None):
+    info(inspect.stack()[0][3] + '()')
+    m = int(np.sqrt(n))
+    pos, adj = generate_lattice(m, False)
     g = igraph.Graph.Adjacency(adj.tolist(), mode=igraph.ADJ_UNDIRECTED)
     g.vs['x'] = pos[:, 0]
     g.vs['y'] = pos[:, 1]
-    # igraph.plot(g, '/tmp/graph.pdf')
     return g
 
 #############################################################
+def create_dummy_graph(n, top, ax=None):
+    info(inspect.stack()[0][3] + '()')
+
+    if top == 'lattice':
+        g = create_lattice(n, ax)
+    elif top == 'grg':
+        radius = 3/n
+        print(radius)
+        g = igraph.Graph.GRG(n, radius=radius)
+
+    plot_graph(g, ax)
+    return g
+
+#############################################################
+def plot_graph(g, ax=None):
+    info(inspect.stack()[0][3] + '()')
+    if not ax: return
+    xs = g.vs['x']
+    ys = g.vs['y']
+    ax.scatter(xs, ys)
+
+    segments = np.zeros((g.ecount(), 2, 2), dtype=float)
+    for i, e in enumerate(g.es()):
+        segments[i, 0, :] = [xs[e.source], ys[e.source]]
+        segments[i, 1, :] = [xs[e.target], ys[e.target]]
+    col = matplotlib.collections.LineCollection(segments)
+    ax.add_collection(col)
+
+#############################################################
 def build_spatial_index(network):
+    info(inspect.stack()[0][3] + '()')
     pass
 
 #############################################################
@@ -133,7 +168,18 @@ def parse_osm_network(osmpath):
     return None
 
 ##########################################################
-def get_nearest_road(point, network):
+def is_inside_segment(point, versor, p0, p1, eps=0.001):
+    """Assuming p=p0+versor*t"""
+    if versor[0] == 0: i = 1
+    else: i = 0
+
+    trange = sorted([0, (p1[i] - p0[i] / versor[i])])
+    t = (point[i] - p0[i]) / versor[i]
+    if (t < trange[0] - eps) or (t > trange[1] + eps): return False
+    else: return True
+
+##########################################################
+def get_nearest_road(point, network, ax=None):
     """Get id of the nearest street in the network. If there are two streets with same distance, get the first I find
 
     Args:
@@ -144,15 +190,54 @@ def get_nearest_road(point, network):
     int: from list of network
     """
     info(inspect.stack()[0][3] + '()')
-    for e in network.es:
+    dists = np.zeros(network.ecount(), dtype=float)
+    refpoints = np.zeros((network.ecount(), 2), dtype=float)
+
+    for i, e in enumerate(network.es):
         p0 = np.array([network.vs[e.source]['x'], network.vs[e.source]['y']])
-        p1 = np.array([network.vs[e.target]['x'], network.vs[e.source]['y']])
-        versor = (p1 - p0) / np.linalg.norm(p1 - p0)
-        # TODO I am here
-    # print(p0, p1)
-    # for v in network.vs:
-        # print(v['x'], v['y'])
-    return -1
+        p1 = np.array([network.vs[e.target]['x'], network.vs[e.target]['y']])
+
+        versor = (p1 - p0) / np.linalg.norm(p1 - p0) # versor
+
+        projpoint = get_point_projection(point, p0, versor)
+        inside = is_inside_segment(projpoint, versor, p0, p1, eps=0.001)
+        candidates = [p0, p1]
+        if inside: candidates.append(projpoint)
+        print(p0, p1, projpoint, inside)
+        localdists = cdist([point], candidates)[0]
+        nearestid = np.argmin(localdists)
+        dists[i] = localdists[nearestid]
+        refpoints[i] = candidates[nearestid]
+
+    edgeid = np.argmin(dists)
+    refpoint = refpoints[edgeid]
+    dist = dists[edgeid]
+
+    if ax:
+        c = [np.random.rand(3,)]
+        ax.scatter(point[0], point[1], c=c, s=20)
+        ax.scatter(refpoint[0], refpoint[1], c=c, s=15)
+    return edgeid, refpoint, dist
+
+##########################################################
+def get_point_projection(point, p0, versor):
+    """Point projection into line
+
+    Args:
+    point, p0, versor(str): output dir
+
+    Returns:
+    ret
+    """
+    info(inspect.stack()[0][3] + '()')
+    versororth = np.array([-versor[1], versor[0]])
+    a = np.concatenate([
+        versororth.reshape(len(versororth), 1),
+        -versor.reshape(len(versor), 1),
+        ], axis=1)
+    b = p0 - point
+    x = np.linalg.solve(a, b)
+    return point + x[0] * versororth
 
 ##########################################################
 def get_road_angle(roadid, network):
@@ -220,26 +305,35 @@ def run_experiment(params_):
     info(inspect.stack()[0][3] + '()')
     cityname = 'sao paulo'
     viewangle = np.pi / 4
+    nvertices = 5
 
     # given alpha=heading
     osmfile = get_osm_file(cityname)
     # network = parse_osm_network(osmfile)
 
-    network = create_dummy_graph()
-    points = [[0, 0]]
+    nrows = 1;  ncols = 1
+    figscale = 5
+    fig, ax = plt.subplots(nrows, ncols,
+            figsize=(ncols*figscale, nrows*figscale))
+
+    network = create_dummy_graph(nvertices, 'grg', ax)
+    points = [[1.0, .5]]
+    # points = np.random.rand(5, 2) + 0.05
 
     points = np.array(points)
 
     # tree = build_spatial_index(network)
     for point in points:
-        fig, ax = plt.subplots()
-        roadid = get_nearest_road(point, network)
+        roadid, refpoint, dist = get_nearest_road(point, network, ax)
+        # print(point, roadid, refpoint, dist)
         roadangle = get_road_angle(roadid, network)
 
         plot_vectors(viewangle, roadangle, ax)
         plot_top_view(point, viewangle, roadangle, network, ax)
         plot_street_view(point, viewangle, roadangle, network, ax)
-        plt.close()
+        # plt.close()
+
+    plt.savefig(pjoin(params_['outdir'], 'graph.pdf'))
 
 ##########################################################
 def main():
@@ -254,6 +348,9 @@ def main():
 
     aux = list(product([args.outdir]))
 
+    seed = 0
+    np.random.seed(seed)
+    random.seed(seed)
     params = []
     for i, row in enumerate(aux):
         params.append(dict(outdir = row[0],))
